76dbd9f06e4365b5382c58d35b3be1149ea089a9
Sashi <phanedra.sasidhar@gmail.com>
2026-06-12 10:19:47 +0530

    updated config and conftest.py files

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 class Config:
-    email="saship@abc.com"
-    password="Sairam@123"
+    email="pavanol@abc.com"
+    password="test@123"
 
-    invalid_email="saship344@abc.com"
-    invalid_password="Sairam@123"
+    invalid_email="pavanol123@abc.com"
+    invalid_password="test@123xyz"
+
     product_name = "MacBook"
     product_quantity = "1"
-    total_price = "$500.00"
+    total_price = "$602.00"
